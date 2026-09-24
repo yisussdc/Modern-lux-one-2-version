48280c0d11289547a0f16e8729998eadc10a65c8
yisussdc <yisuss101297@gmail.com>
2026-09-24 10:24:46 -0400

    actualizaciones

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             try {
                 // Petición a tu API local
-                const response = await fetch('http://127.0.0.1:8000/api/solicitar-inspeccion', {
+                const response = await fetch('https://backend-inspecciones-72xj.onrender.com', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(datosFormulario)
